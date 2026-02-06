c2cf0128f5d6fefc961815a08e685aaaad33337a
Funmi123 <ajanakueniola@gmail.com>
2026-02-06 02:37:37 +0100

    added associate public ip address to be true for connection

--- terraform/main.tf
+++ terraform/main.tf
@@ -77,6 +77,7 @@ resource "aws_instance" "amazon_linux" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = var.instance_type
   key_name      = var.key_name
+  associate_public_ip_address = true
   vpc_security_group_ids = [aws_security_group.server_sg.id]
 
   tags = {
@@ -90,6 +91,7 @@ resource "aws_instance" "ubuntu" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = var.instance_type
   key_name      = var.key_name
+  associate_public_ip_address = true
   vpc_security_group_ids = [aws_security_group.server_sg.id]
 
   tags = {
